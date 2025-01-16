a59b2454d3bbb32a485e9ac06c162917ec110ff5
tylerasa <sarpongasaresylvester@gmail.com>
2025-01-16 17:11:48 +0000

    chore: exposing modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { TransactionsModule } from './transactions/transactions.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule],
+  imports: [AuthModule, PrismaModule, TransactionsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
